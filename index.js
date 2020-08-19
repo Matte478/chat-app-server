b927a10b4483594d57301440dbee76c39fd3ab18
David Spak <david.spak478@gmail.com>
2020-08-19 23:06:20 +0200

    ENV port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
 
-const port = 5000
+const PORT = process.env.PORT || 5000
 
 const router = require('./router')
 
@@ -62,6 +62,6 @@ io.on('connection', (socket) => {
   })
 })
 
-server.listen(port, () => {
+server.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
